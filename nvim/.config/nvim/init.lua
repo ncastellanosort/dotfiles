609a8bb26b47f131195ea8d2f98bc7f138f8646a
ncastellanosort <nicolasfelipe00234@gmail.com>
2025-12-29 20:59:12 -0500

    chore: remap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -217,6 +217,8 @@ vim.g.netrw_banner = 0
 -- Keybindings
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<leader>i", vim.lsp.buf.code_action, { noremap = true, silent = true })
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
